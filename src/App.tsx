245b9915acca68eba5434c6b94a7be6ea740c987
MortezaMoppe <116831313+MortezaMoppe@users.noreply.github.com>
2025-02-26 15:36:38 +0100

    uppdaterad så man kan se order och kvitto

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import { Menu } from "./components/Menu";
 import { Cart } from "./components/Cart";
 import { Navbar } from "./components/Navbar";
+import { Order } from "./components/Order";
+import { Receipt } from "./components/Receipt";
 
 export const App = () => (
   <>
@@ -9,6 +11,8 @@ export const App = () => (
     <Routes>
       <Route path="/" element={<Menu />} />
       <Route path="/cart" element={<Cart />} />
+      <Route path="/order" element={<Order />} />
+      <Route path="/receipt" element={<Receipt />} />
     </Routes>
   </>
 );
