a78f030acc668fe4e0327c13f77f4316daf6fd5f
MortezaMoppe <116831313+MortezaMoppe@users.noreply.github.com>
2025-02-26 16:39:27 +0100

    lägger till api i store

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,13 +2,17 @@ import { configureStore } from "@reduxjs/toolkit";
 import menuReducer from "../features/menu/menuSlice";
 import cartReducer from "../features/cart/cartSlice";
 import orderReducer from "../features/order/orderSlice";
+import { foodtruckApi } from "../api/foodtruckApi";
 
 export const store = configureStore({
   reducer: {
     menu: menuReducer,
     cart: cartReducer,
     order: orderReducer,
+    [foodtruckApi.reducerPath]: foodtruckApi.reducer
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(foodtruckApi.middleware)
 });
 
 export type RootState = ReturnType<typeof store.getState>;
