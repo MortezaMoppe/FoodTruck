468d82997332621e19b4b34168d687ccaf1e3260
MortezaMoppe <116831313+MortezaMoppe@users.noreply.github.com>
2025-02-26 15:29:43 +0100

    Nu ska antal produkter visas i varukorgen i navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,16 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { RootState } from "../app/store";
 
-export const Navbar = () => (
-  <nav className="p-4 bg-gray-800 text-white flex justify-between">
-    <Link to="/">Morteza Foodtruck</Link>
-    <Link to="/cart">Varukorg</Link>
-  </nav>
-);
+export const Navbar = () => {
+  const cartCount = useSelector((state: RootState) =>
+    state.cart.items.reduce((acc, item) => acc + item.quantity, 0)
+  );
+
+  return (
+    <nav className="p-4 bg-gray-800 text-white flex justify-between">
+      <Link to="/">Meny</Link>
+      <Link to="/cart"> ({cartCount})</Link>
+    </nav>
+  );
+};
