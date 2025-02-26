ebd5c5c6699f939273c9e793f4fa5961c7e6dfe9
MortezaMoppe <116831313+MortezaMoppe@users.noreply.github.com>
2025-02-26 20:02:22 +0100

    Heroicon installerad, Finare varukorg med siffror istället för bara siffror :D

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { RootState } from "../app/store";
+import { ShoppingCartIcon } from "@heroicons/react/24/outline";
 
 export const Navbar = () => {
   const cartCount = useSelector((state: RootState) =>
@@ -8,9 +9,17 @@ export const Navbar = () => {
   );
 
   return (
-    <nav className="p-4 bg-gray-800 text-white flex justify-between">
-      <Link to="/">Meny</Link>
-      <Link to="/cart"> ({cartCount})</Link>
+    <nav className="p-4 bg-gray-800 text-white flex justify-between items-center">
+      <Link to="/" className="text-lg font-semibold">Meny</Link>
+      
+      <Link to="/cart" className="relative flex items-center space-x-2">
+        <ShoppingCartIcon className="h-6 w-6 text-white" />
+        {cartCount > 0 && (
+          <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs font-bold w-5 h-5 flex items-center justify-center rounded-full">
+            {cartCount}
+          </span>
+        )}
+      </Link>
     </nav>
   );
 };
