71260451232308b10a8b60c4859a4bad2cc47f05
MortezaMoppe <116831313+MortezaMoppe@users.noreply.github.com>
2025-02-28 14:23:03 +0100

    visning av varukorg samt innehåll

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation} from "react-router-dom";
 import { useSelector } from "react-redux";
 import { RootState } from "../app/store";
 import { ShoppingCartIcon } from "@heroicons/react/24/outline";
@@ -8,18 +8,24 @@ export const Navbar = () => {
     state.cart.items.reduce((acc, item) => acc + item.quantity, 0)
   );
 
+  const location = useLocation();
+  const hideCartNumber = ["/cart", ].includes(location.pathname);
+  const hideCartIcon = ["/eta", "/receipt"].includes(location.pathname);
+
   return (
     <nav className="p-4 bg-gray-800 text-white flex justify-between items-center">
       <Link to="/" className="text-lg font-semibold">Meny</Link>
       
-      <Link to="/cart" className="relative flex items-center space-x-2">
-        <ShoppingCartIcon className="h-6 w-6 text-white" />
-        {cartCount > 0 && (
-          <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs font-bold w-5 h-5 flex items-center justify-center rounded-full">
-            {cartCount}
-          </span>
-        )}
-      </Link>
+      {!hideCartIcon && ( 
+        <Link to="/cart" className="relative flex items-center space-x-2">
+          <ShoppingCartIcon className="h-6 w-6 text-white" />
+          {!hideCartNumber && cartCount > 0 && ( 
+            <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs font-bold w-5 h-5 flex items-center justify-center rounded-full">
+              {cartCount}
+            </span>
+          )}
+        </Link>
+      )}
     </nav>
   );
 };
